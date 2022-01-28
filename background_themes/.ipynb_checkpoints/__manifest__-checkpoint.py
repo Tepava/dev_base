52a9ef6892d953560883a3ceabd56b2714a7e4be
Tepava <mehdi@pacific-erp.com>
2022-01-28 22:54:01 +0000

    Initialisation dossier custom

---
+++ background_themes/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "background_themes",
+    'summary': """
+        Odoo theme personalization""",
+
+    'description': """
+        Long description of module's purpose
+    """,
+    'author': "Pacific-ERP",
+    'website': "https://www.pacific-erp.com",
+    'category': 'Application',
+    'version': '0.1',
+    'depends': ['base'],
+    'data': [
+        'views/settings.xml',
+    ],
+}
